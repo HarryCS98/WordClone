2cc13341faf5ede7ac93be62dda1a7d03a6a9ad2
Harry <harryjowatts@gmail.com>
2021-06-08 04:03:36 +0100

    Added Title to MainWindow Added label for word count

--- WordClone/src/application/Main.java
+++ WordClone/src/application/Main.java
@@ -21,6 +21,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root,800,500);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("WordClone");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
