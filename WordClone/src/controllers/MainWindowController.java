086d28c1121d4654532591798885321e65d21efd
Harry <harryjowatts@gmail.com>
2021-06-10 04:43:47 +0100

    Changed the save button to Save As this now opens a new window that defaults to the my Documents directory and has a default name of document.txt and then when the save button is pressed the document will now be saved

--- WordClone/src/controllers/MainWindowController.java
+++ WordClone/src/controllers/MainWindowController.java
@@ -7,63 +7,57 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 import models.FileModel;
 
 public class MainWindowController {
 
 	FileModel filemodel = new FileModel();
-	
-	//Make reference to the TextArea in MainWindowView.FXML so we can access the text in the application
-	
+
+	// Make reference to the TextArea in MainWindowView.FXML so we can access the
+	// text in the application
+
 	@FXML
 	TextArea content = new TextArea();
-	
-	
+
 	@FXML
 	Button testButton = new Button();
-	
+
 	FileChooser fileChooser = new FileChooser();
-	
-	
+
 	Stage stage = new Stage();
-	
-	
+
 	public void load(Stage primaryStage) {
-		
+
 		stage = primaryStage;
-		
+
 	}
-	
-	
-	
-	
-	
+
 	public void saveFile() throws IOException {
 		
 		
-		String fileName = "test.txt";
-		
-		 filemodel.saveFile(fileName, content.getText());
-		
-	}
-	
+//		  Set to to the users home so user there name
+		  String userHomePath = System.getProperty("user.home"); 
+		  File userHome = new File(userHomePath); // uses the corect path separator for the OS File videos
+//		  Create new file object that is user home then Documents
+		  File mydocuments = new File (userHome,"Documents");
+//		  Open the file chooser in the my Documents directory
+		  fileChooser.setInitialDirectory(mydocuments);
+//		  Set the default name of the file that is going to be saved to Document.txt
+		  fileChooser.setInitialFileName("Document.txt");
 
-	
-	public void test_openFileLocation() throws IOException {
-		
-		/*
-		 * String userHomePath = System.getProperty("user.home"); File userHome = new
-		 * File(userHomePath); // uses the corect path separator for the OS File videos
-		 * = new File(userHome, "Documents"); Desktop.getDesktop().open(videos);
-		 */
-        
-		
-		File selectedFile = fileChooser.showOpenDialog(stage);
-		
-      
-		
+		  fileChooser.getExtensionFilters().addAll(new ExtensionFilter("All Files", "*.*"));
+		  
+ //		  Open then save verion of the file Chooser
+		  File selectedFile = fileChooser.showSaveDialog(stage);
+		  
+//		  Stop a null point exception error that accures if the user presses the cancel button in the Save Dialog box
+		  if(selectedFile != null) {
+			  filemodel.saveFile(selectedFile.getAbsolutePath(), content.getText());
+		  }
+		 
+
 	}
 
-	
 }
